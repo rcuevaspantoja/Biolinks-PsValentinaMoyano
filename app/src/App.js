914af3168b1e6bcc62b53c13d0568c02c6583178
Rodolfo Cuevas <50083009+rcuevaspantoja@users.noreply.github.com>
2022-12-21 05:05:40 +0000

    falta fondo, falta cards, falta todo

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Enlaces from './componentes/Enlaces'
+
+function App() {
+  return (
+    <div className="App">
+
+      <Enlaces/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/componentes/Enlaces.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Enlaces.css'
+
+function Enlaces() {
+
+  return (
+
+    <div className='Enlaces'>
+        <h1>Ps. Valentina Moyano</h1>
+    </div>
+  )
+}
+
+export default Enlaces
