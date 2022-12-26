66a263d5490f4db2d568e4c747673697027c9eb1
Rodolfo Cuevas <50083009+rcuevaspantoja@users.noreply.github.com>
2022-12-26 11:55:39 +0000

    26-12-22

--- app/src/componentes/Enlaces.js
+++ app/src/componentes/Enlaces.js
@@ -1,39 +1,68 @@
 import React from 'react'
 import './Enlaces.css'
 import Card from '@mui/material/Card';
-import { Button, Typography } from '@mui/material';
-import CardContent from '@mui/material/CardContent';
-import './Tarjetas.css';
+import { Button } from '@mui/material';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
 
 function Enlaces() {
 
   const Tarjeta = (props) => {
-    return <Button>
-            <Card sx={{ minWidth: 500 }} className='Card'>
-              <CardContent>
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    {props.children}
-                  </Typography>
-              </CardContent>
+    return <Button className='Card'>
+            <Card sx={{ minWidth: 270 }} 
+               style={{
+                borderRadius: 15,
+                height: "60px",
+                backgroundColor: "pink",
+                border: "3px solid rgb(241, 185, 43)",
+              }}
+              variant="contained" 
+            >
+
+              <h2> {props.children} </h2>
+
             </Card>     
           </Button>
   }
-
 
   return (
   <>
     <div className='Enlaces'>
         <h1 className='h1-div'>Ps. Valentina Moyano</h1>
+        <div className='fondo-blanco'>
 
-        <Tarjeta onClick={ console.log() }>Instagram</Tarjeta>
-        <Tarjeta onClick={ console.log() }>TikTok</Tarjeta>
-        <Tarjeta onClick={ console.log() }>Whatsapp</Tarjeta>
+          <Link href="https://instagram.com/ps.valentinamoyano" underline="none">
+            <Tarjeta>Instagram</Tarjeta>
+          </Link>
+          
+          <Link href="https://tiktok.com/@ps.valentinamoyano" underline="none">
+            <Tarjeta>TikTok</Tarjeta>
+          </Link>
 
+          <Link href="" underline="none">
+            <Tarjeta>Whatsapp</Tarjeta>
+          </Link>
+
+          <div className='subscribete'>
+            <h2>
+              Para una notificación de horas disponibles, subscribete aquí:
+            </h2>
+            <TextField id="outlined-basic" label="Email" variant="outlined" size="small" />
+
+            <div className='botonsub'>
+              <Button
+                  style={{
+                        borderRadius: 3,
+                        backgroundColor: "rgb(241, 185, 43)",
+                        fontSize: "13px",
+                        color: "black"
+                  }}
+              variant="contained">Suscribirse</Button>
+            </div>     
+          </div>
+        </div>
     </div>
-
-    
   </>  
-
   )
 }
 
